2a14cc8d01c432628ef53c9c4cf8cbf564d59ed1
kirill-multilogin <kirill.kiselev@multilogin.com>
2024-05-12 13:14:28 +0300

    change secret_key in taski to txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+with open('../secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 
 DEBUG = True
 
